a3686445d7080f1635f825f551bf8c27b6ff1343
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-10-20 21:00:39 -0700

    deleted mockup in index.js

--- test/index.js
+++ test/index.js
@@ -15,30 +15,3 @@ global.URL = window.URL;
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
-
-function storageMock() {
-    var storage = {};
-
-    return {
-        setItem: function (key, value) {
-            storage[key] = value || '';
-        },
-        getItem: function (key) {
-            return key in storage ? storage[key] : null;
-        },
-        removeItem: function (key) {
-            delete storage[key];
-        },
-        get length() {
-            return Object.keys(storage).length;
-        },
-        key: function (i) {
-            var keys = Object.keys(storage);
-            return keys[i] || null;
-        }
-    };
-}
-
-Object.defineProperty(window, 'localStorage', {
-    value: storageMock(),
-});
